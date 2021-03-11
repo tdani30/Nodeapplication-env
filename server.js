d836676c1d0f1b00b5b3673ff6f95845b357edf3
Talha <syedtalha30@gmail.com>
2021-03-11 15:16:03 +0400

    COMMIET

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 3080;
+      const PORT = process.env.PORT || 8081;
 
 // place holder for the data
 const users = [];
